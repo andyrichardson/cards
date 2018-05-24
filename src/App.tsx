fa3ef56db41e88192a49be68b73f2a6f575b71e8
Andy Richardson <andy.john.richardson@gmail.com>
2018-05-24 21:18:19 +0100

    add layout styling for page

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,45 @@ class App extends React.Component {
   public render() {
     return (
       <Provider {...stores}>
-        <>
-          <Deck />
+        <AppContainer>
+          <DeckContainer>
+            <Deck />
+          </DeckContainer>
           <Table className="table">
             {stores.deckStore.table.map((card, index) => (
               <Card key={index} rank={card.rank} suit={card.suit} />
             ))}
           </Table>
-        </>
+        </AppContainer>
       </Provider>
     );
   }
 }
 
-const Table = styled.div`
+const AppContainer = styled.div`
+  align-items: center;
   display: flex;
+  height: 100%;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`;
+
+const DeckContainer = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  min-width: 400px;
+  padding: 200px 0;
+`;
+
+const Table = styled.div`
+  display: inline-flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  justify-content: center;
+
   div {
     margin: 10px;
   }
